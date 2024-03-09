2edce05bcb314811b4887ee719b920e84dfcfcec
Sabrina Matos <sabrinamatosalmeida2@gmail.com>
2024-03-09 17:43:00 -0300

    criação do JS

---
+++ control/cadastro.js
@@ -0,0 +1,10 @@
+const main = document.getElementsByClassName("main")[0]
+const botaoSalvar= document.getElementById("botaosalvar")
+
+botaoSalvar.addEventListener('click',function(){
+    const nome= document.getElementById("nome")
+    const dica= document.getElementById("textarea")
+    const email= document.getElementById("email")
+    const celular= document.getElementById("celular")
+
+})
