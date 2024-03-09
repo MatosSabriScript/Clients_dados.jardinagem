b21942bb3406d529b09a5b0e3b1e46d698c44601
Sabrina Matos <sabrinamatosalmeida2@gmail.com>
2024-03-09 18:24:04 -0300

    salvamento de dados

--- control/cadastro.js
+++ control/cadastro.js
@@ -1,10 +1,23 @@
 const main = document.getElementsByClassName("main")[0]
 const botaoSalvar= document.getElementById("botaosalvar")
+// console.log("r")
 
-botaoSalvar.addEventListener('click',function(){
+botaoSalvar.addEventListener('click',function(e){
     const nome= document.getElementById("nome")
     const dica= document.getElementById("textarea")
     const email= document.getElementById("email")
     const celular= document.getElementById("celular")
 
-})
+     if(nome.value.length >=3 && dica.value.length >=30 && celular.value.length>=11 && email.value.length>=11){
+        e.preventDefault()
+        console.log(nome.value)
+        console.log(celular.value)
+        console.log(dica.value)
+        console.log(email.value)
+     }
+}) 
+
+
+    
+
+  
