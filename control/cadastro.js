f95e21f38a5d9872a83bb5df822ff5be5289dd3d
Sabrina Matos <sabrinamatosalmeida2@gmail.com>
2024-03-09 20:09:09 -0300

    criação do botão editar

--- control/cadastro.js
+++ control/cadastro.js
@@ -11,10 +11,21 @@ botaoSalvar.addEventListener('click',function(e){
      if(nome.value.length >=3 && dica.value.length >=30 && celular.value.length>=11 && email.value.length>=11){
         e.preventDefault()
         criarDica(nome.value,dica.value,email.value,celular.value)
+        apagarInputs()
      }
 }) 
 
+function apagarInputs(){
+    const nome= document.getElementById("nome")
+    const dica= document.getElementById("textarea")
+    const email= document.getElementById("email")
+    const celular= document.getElementById("celular")
+    nome.value=''
+    dica.value=''
+    email.value=''
+    celular.value=''
 
+}
 
 class Dica {
     constructor(nome,dica,email,celular){
@@ -68,8 +79,42 @@ function criarElemento(dica){
     })
 
     div.appendChild(botaoApagar)
+
+    const botaoEditar= document.createElement('button')
+    botaoEditar.innerHTML= "Editar"
+    botaoEditar.addEventListener('click',function(){
+        const nomeElemento= document.getElementById("nome")
+        const dicaElemento= document.getElementById("textarea")
+        const emailElemento= document.getElementById("email")
+        const celularElemento= document.getElementById("celular")
+        nomeElemento.value=dica.nome
+        dicaElemento.value=dica.dica
+        emailElemento.value=dica.email
+        celularElemento.value=dica.celular
+
+
+        main.removeChild(div)
+        
+
+        const index = dicasSalvas.findIndex(dicaIndex => {
+            return dicaIndex.nome === dica.nome &&
+            dicaIndex.dica === dica.dica &&
+            dicaIndex.email === dica.email &&
+            dicaIndex.celular === dica.celular 
+        })
+        dicasSalvas.splice(index, 1)
+    })
+
+    div.appendChild(botaoEditar)
+
+
+
+
+
+
+
     main.appendChild(div)
 }
 
-
+ 
   
